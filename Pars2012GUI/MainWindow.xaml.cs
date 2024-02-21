db43a5443936398046727dd0f484b170f5a37e6f
sinka.jozsef <sinka.jozsef@57-08>
2024-02-21 10:15:01 +0100

    Add project files.

---
+++ Pars2012GUI/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pars2012GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Versenyző> versenyzok = new List<Versenyző>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            //Adatfeltöltés
+            File.ReadAllLines("../../../Selejtezo2012.txt").Skip(1).ToList().ForEach(x => versenyzok.Add(new Versenyző(x)));
+
+            cbValaszt.ItemsSource = versenyzok.DistinctBy(x=>x.Nev).Select(x=>x.Nev);
+            cbValaszt.SelectedItem = "Pars Krisztián";
+
+        }
+
+        private void cbValaszt_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Versenyző kivalasztott = versenyzok.Where(n => n.Nev == cbValaszt.SelectedItem.ToString()).First();
+            lblCsoport.Content = "Csoport: " + kivalasztott.Csoport;
+            lblNemzet.Content = "Nemzet: " + kivalasztott.Nemzet;
+            lblSorozat.Content = "Sorozat: "+ string.Join(';', kivalasztott.DobasokErdeti);
+            lblEredmeny.Content = "Eredmény: " + kivalasztott.Eredmény;
+            imgZaszlo.Source = new BitmapImage(new Uri($"Images/{kivalasztott.Kód}.png", UriKind.Relative));
+        }
+    }
+}
---
+++ Pars2012GUI/Versenyző.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pars2012GUI
+{
+    public class Versenyző
+    {
+        string nev;
+        string csoport;
+        string nemzetTeljes;
+        string kód;
+        string nemzet;
+        List<double> dobasok = new List<double>(); // X = -1     - = -2
+        List<string> dobasokErdeti = new List<string>();
+        bool automatikusanTovabb;
+        double eredmény;
+
+
+        public Versenyző(string sor)
+        {
+            List<string> mezo = sor.Split(';').ToList();
+            List<double> lista = new List<double>();
+            List<string> listaErdeti = new List<string>();
+            for (int i = 3; i < mezo.Count(); i++)
+            {
+                if (mezo[i] == "-")
+                {
+                    lista.Add(-2);
+                }
+                else if (mezo[i] == "X")
+                {
+                    lista.Add(-1);
+                }
+                else
+                {
+                    lista.Add(double.Parse(mezo[i]));
+                }
+                listaErdeti.Add(mezo[i]);
+            }
+
+            this.nev = mezo[0];
+            this.csoport = mezo[1];
+            this.nemzetTeljes = mezo[2];
+            this.dobasok = lista;
+            this.dobasokErdeti = listaErdeti;
+
+            automatikusanTovabb = dobasok.Contains(-2);
+            eredmény = dobasok.Max(x => x);
+
+            this.kód = mezo[2][^4..^1];
+            this.nemzet = mezo[2][0..^6];
+        }
+
+
+        public string Nev { get => nev; }
+        public string Csoport { get => csoport; }
+        public string NemzetTeljes { get => nemzetTeljes; }
+        public string Kód { get => kód; }
+        public string Nemzet { get => nemzet; }
+        public List<double> Dobasok { get => dobasok; }
+
+        public bool AutomatikusanTovabb { get => automatikusanTovabb; }
+        public double Eredmény { get => eredmény; }
+        public List<string> DobasokErdeti { get => dobasokErdeti; }
+
+        public override string ToString()
+        {
+            return $"{nev};{csoport};{nemzetTeljes};{string.Join(';', dobasokErdeti)}";
+        }
+    }
+}
